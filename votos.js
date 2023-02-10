ee5c311b0b69a584207c8822522952863afd6027
CATHARINE OLIVEIROS DE OLIVEIRA <catharine.ooliveira@senacsp.edu.br>
2023-02-10 09:42:55 -0300

    fist commit

---
+++ votos.js
@@ -0,0 +1,23 @@
+let rosa;
+let azul;
+let resultado;
+let numv1;
+let numv2;
+
+function calcular() {
+    let escolha = prompt("Escolha uma cor");
+
+
+switch (escolha) {
+    case "rosa":
+        soma();
+        break;
+    case "azul":
+      soma();
+        break;
+    default:
+        break;
+}
+
+}
+resultado = (numv1 + numv2);
